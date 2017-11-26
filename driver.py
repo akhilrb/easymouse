32419a2b3bba79aa3ddcbb00c34aa05ffa46bde5
Akhil Raj Baranwal <akhil.r.baranwal@gmail.com>
2017-11-26 18:43:29 +0530

    colour-coded console

--- driver.py
+++ driver.py
@@ -4,39 +4,57 @@ import subprocess
 import pyautogui
 from time import sleep
 from enum import IntEnum
-
 ###########################################################################################################################################
 # Priority definitions for output
 class Priority(IntEnum):
-    SILENT = 5
-    ERROR = 4
-    SETTING = 3
+    SILENT = 6
+    ERROR = 5
+    SETTING = 4
+    CONFIG = 3
     EVENT = 2
     GUI = 1
     ALL = 0
 
-# Default variables
+# Variables
 BAUD_LIST = [9600, 19200, 38400, 57600, 115200, 230400]
 BAUDRATE = 9600
 PORT = ""
 PORT_LIST = []
 serialReceiver = serial.Serial()
 interv = 0.10
-VerbosePriority = Priority.EVENT
+VerbosePriority = Priority.ALL
+
+BLUE =  '\033[1;38;2;32;64;227m'
+RED =   '\033[1;38;2;227;32;32m'
+GREEN = '\033[0;38;2;0;192;0m'
+YELLOW ='\033[0;38;2;192;192;0m'
+NC =    '\033[0m'
 
 ###########################################################################################################################################
-# Prints messages according to priority
-def consolePrint(msg, priority):
-    #max error length is 64 characters
-    #msg = '|' + msg + ' '*(66 - len(msg)) + '|'
-    if priority >= VerbosePriority:
-        if priority == Priority.GUI:
-            print(msg)
-        else:    
-            #print("\n -"+str('-'*64)+"-\n" + msg + "\n -"+str('-'*64)+"-\n")
-            print(str('-'*len(msg)) + '\n' + msg + '\n' + str('-'*len(msg)))
+# console messages according to priority
+def consolePrint(msg, *options):
+    # check whether only priority has been specified
+    if len(options) == 1:
+        priority = options[0]
+        if priority >= VerbosePriority:
+            if priority == Priority.GUI:
+                print('>>> ' + msg)
+            elif priority == Priority.ERROR:
+                print(RED + '>>> ' + msg + NC)
+            elif priority == Priority.SETTING:
+                print(BLUE + '>>> ' + msg + NC)
+            elif priority == Priority.EVENT:
+                print(YELLOW + '>>> ' + msg + NC)
+    # check whether both have been specified
+    elif len(options) == 2:
+        priority = options[0]
+        colour = options[1]
+        if priority >= VerbosePriority:
+            print(colour + '>>> ' + msg + NC)
+    else:
+        if VerbosePriority == Priority.ALL:
+            print('>>> ' + msg)
 
-    
 
 # returns output of a terminal command
 def cmdLine(cmd):
@@ -45,9 +63,9 @@ def cmdLine(cmd):
 
 ###########################################################################################################################################
 '''
-----------------------------------
-SERIAL RECEIVER SPECIFIC FUNCTIONS
-----------------------------------
+-------------------------
+SERIAL SPECIFIC FUNCTIONS
+-------------------------
 '''
 
 # scan ports
@@ -73,12 +91,13 @@ def choosePort():
         # find and eliminate trailing whitespaces
         wsi = str(port).find(' ')
         PORT_LIST.append(str(port)[0:wsi])
-    #subtract 1 to compensate newline
+    # subtract 1 to compensate newline
     for x in range(len(PORT_LIST)-1):
         print (str(x+1)+") "+str(PORT_LIST[x]))
     print (str(len(PORT_LIST))+") Rescan")
     i = int(input("---------------------\nSelect Option #"))
-    #print(PORT_LIST)
+    # print(PORT_LIST)
+    # an extra "newline" is stored in the list, therefore only <, not <=
     if(i < len(PORT_LIST)):
         return PORT_LIST[i-1]
     else:
@@ -92,7 +111,7 @@ def configurePort():
     for x in range(len(BAUD_LIST)):
         print (str(x+1)+") "+str(BAUD_LIST[x]))
     i = int(input("---------------------\nSelect Option #"))
-    if(i < len(BAUD_LIST)):
+    if(i <= len(BAUD_LIST)):
         BAUDRATE = BAUD_LIST[i-1]
     else:
         BAUDRATE = 115200
@@ -102,11 +121,11 @@ def startReceiver():
     global PORT, BAUDRATE, serialReceiver
     configurePort()
     try:
-        print("\nTrying "+ str(PORT)+" @ " + str(BAUDRATE)+" bps...\t"+ "Max Comm Speed: " + str("%.2f" %(float(BAUDRATE)/8096)) + " kBps")
+        consolePrint("Trying "+ str(PORT)+" @ " + str(BAUDRATE)+" bps...\t"+ "Max Comm Speed: " + str("%.2f" %(float(BAUDRATE)/8096)) + " kBps", Priority.EVENT)
         sleep(1)
         serialReceiver =  serial.Serial(PORT, BAUDRATE)
         sleep(0.5)
-        consolePrint("Driver Running", Priority.EVENT)
+        consolePrint("Driver Running", Priority.EVENT, GREEN)
     except serial.serialutil.SerialException as e:
         consolePrint("No device was detected on " + str(PORT), Priority.ERROR)
         startReceiver()
@@ -115,21 +134,21 @@ def startReceiver():
 def restartReceiver():
     global PORT, BAUDRATE, serialReceiver
     try:
-        consolePrint("Attempting to restart driver automatically", Priority.EVENT)
-        print("Trying "+ str(PORT)+" @ " + str(BAUDRATE)+" bps...\t"+ "Max Comm Speed: " + str("%.2f" %(float(BAUDRATE)/8096)) + " kBps")
+        consolePrint("Attempting to restart", Priority.EVENT)
+        #consolePrint("Trying "+ str(PORT)+" @ " + str(BAUDRATE)+" bps...\t"+ "Max Comm Speed: " + str("%.2f" %(float(BAUDRATE)/8096)) + " kBps", Priority.EVENT)
         sleep(1)
         serialReceiver =  serial.Serial(PORT, BAUDRATE)
-        consolePrint("Driver Running", Priority.EVENT)
+        consolePrint("Driver Running", Priority.EVENT, GREEN)
         manageDevice(' ')
     except serial.serialutil.SerialException as e:
-        consolePrint("Port collapsed. Device disconnected unexpectedly from " + str(PORT), Priority.ERROR)
+        consolePrint("Port collapsed. Device disconnected unexpectedly", Priority.ERROR)
         serialReceiver.close()
         startReceiver()
 
 '''
-----------------------------------
-SERIAL RECEIVER SPECIFIC FUNCTIONS
-----------------------------------
+-------------------------
+SERIAL SPECIFIC FUNCTIONS
+-------------------------
 '''
 ###########################################################################################################################################
 '''
@@ -152,6 +171,7 @@ def manageDevice(ch):
             msg = "Clicking " + str(nclicks) + " times with interval " + str("%.2f" %interv) + " sec"
             consolePrint(msg, Priority.GUI)
             pyautogui.click(clicks = nclicks, interval = interv)
+            serialReceiver.write(b'x')
         except:
             pass
     if ch == 'c':
@@ -159,16 +179,27 @@ def manageDevice(ch):
             x, y = pyautogui.position()
             msg = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
             consolePrint(msg, Priority.GUI)
+            serialReceiver.write(b'x')
+        except:
+            pass
+    if ch == 'x':
+        try:
+            x, y = pyautogui.position()
+            pyautogui.moveTo(min(width,x),min(height,y))
+            consolePrint(msg, Priority.GUI)
+            serialReceiver.write(b'x')
         except:
             pass
     if ch == 'I':
         interv = min(interv + 0.02, 0.50)
         msg = "Click Interval: %.2f seconds" %interv
         consolePrint(msg, Priority.SETTING)
+        serialReceiver.write(b'x')
     if ch == 'D':
         interv = max(interv - 0.02, 0.04)
         msg = "Click Interval: %.2f seconds" %interv
         consolePrint(msg, Priority.SETTING)
+        serialReceiver.write(b'x')
 
 '''
 -----------------
@@ -198,7 +229,7 @@ if serialReceiver.isOpen():
         except UnicodeDecodeError as ude:
             # maybe we entered here because of wrong message relay and not a baud mismatch
             try:
-                consolePrint("Corrupt Bytes received",Priority.ERROR)
+                consolePrint("Corrupt byte received",Priority.ERROR)
                 serialReceiver =  serial.Serial(PORT, BAUDRATE)
                 x = (serialReceiver.read()).decode("utf-8")
             except UnicodeDecodeError as ude:
@@ -210,13 +241,14 @@ if serialReceiver.isOpen():
         except Exception as e:
             # something might have happened
             consolePrint("Serial Disturbed", Priority.ERROR)
-            sleep(0.25)
+            sleep(0.5)
             # try a quick fix of switching ports
             try:
                 quickScan()
                 PORT = PORT_LIST[0]
                 serialReceiver =  serial.Serial(PORT, BAUDRATE)
+                consolePrint("Serial Stable", Priority.EVENT, GREEN)
                 x = (serialReceiver.read()).decode("utf-8")
             except Exception as e:
-                consolePrint("Port Transferred to " + PORT, Priority.EVENT)
+                consolePrint("Attempting Port Transfer..." + PORT, Priority.EVENT)
                 restartReceiver();
